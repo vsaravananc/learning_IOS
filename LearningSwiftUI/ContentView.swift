00e8ebd66e1fde19a8be4a77973ef02ba671d662
Saravanan v <v.saravanan.c@gmail.com>
2026-01-22 08:00:52 +0530

    Initial Commit

---
+++ LearningSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearningSwiftUI
+//
+//  Created by Saravanan V on 22/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
