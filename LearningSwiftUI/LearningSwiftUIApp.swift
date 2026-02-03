91515e1ffa82cc3ca45c8cabdbe7c9e1306f1a44
Saravanan v <v.saravanan.c@gmail.com>
2026-02-03 20:45:39 +0530

    completed

--- LearningSwiftUI/LearningSwiftUIApp.swift
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct LearningSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack{
+                SplashView()
+            }
         }
     }
 }
