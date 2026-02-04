88fd79b086f5f6e0ea2d206ce41ddf346a9a0d3c
Saravanan v <v.saravanan.c@gmail.com>
2026-02-04 20:47:33 +0530

    add new screen in detail view

---
+++ LearningSwiftUI/DetailView.swift
@@ -0,0 +1,19 @@
+//
+//  Detail.swift
+//  LearningSwiftUI
+//
+//  Created by Saravanan V on 04/02/26.
+//
+
+import SwiftUI
+
+struct DetailView : View {
+    var body : some View {
+        
+        ScrollView{
+            
+        }.navigationBarBackButtonHidden(true)
+        
+    }
+    
+}
